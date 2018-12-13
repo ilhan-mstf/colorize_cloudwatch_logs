d9b066d1266586538b14e1d66c3209aeac561d3b
Mustafa ilhan <mil@emakina.com>
2018-12-13 11:25:09 +0300

    Runs ever 1sec, filter order changed to make it more performant

--- colorize.js
+++ colorize.js
@@ -1,14 +1,15 @@
+/*!
+ * License: MIT
+ * Author: Mustafa İlhan
+ * http://ilhan-mstf.github.io/
+ */
+
 'use strict'
 
 let colors = ['#FFD54F', '#BCAAA4', '#90CAF9', '#FFCC80', '#FBE9E7', '#B0BEC5', '#DCE775', '#A5D6A7', '#81C784', '#9FA8DA', '#E0E0E0', '#CFD8DC', '#F48FB1', '#F8BBD0', '#F0F4C3', '#C0CA33', '#CE93D8', '#80CBC4', '#E1BEE7', '#CDDC39', '#C5CAE9', '#EF9A9A', '#FFFF00', '#B2EBF2', '#BDBDBD', '#FFE57F', '#B2DFDB', '#BBDEFB', '#69F0AE', '#FFCDD2', '#9CCC65', '#80DEEA', '#76FF03', '#B2FF59', '#C8E6C9']
 
-function sleep (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
 async function colorizeElement (x, color) {
   x.style.backgroundColor = color
-  // await sleep(50)
   x.className = x.className + ' cw-logs-colorized'
   return x
 }
@@ -21,12 +22,18 @@ function eliminateAlreadyColorizedOnes (x) {
   return !x.className.includes('cw-logs-colorized')
 }
 
+/*
+ * Gets event id from the html
+ */
 function selectEventId (x) {
   return x.innerHTML.replace(/\n/g, ' ').match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/g)[0]
 }
 
+/*
+ * Gets unique id list of events
+ */
 function getIdList (x) {
-  return Array.from(new Set(x.filter(selectStartAndEnd).filter(eliminateAlreadyColorizedOnes).map(selectEventId)))
+  return Array.from(new Set(x.filter(eliminateAlreadyColorizedOnes).filter(selectStartAndEnd).map(selectEventId)))
 }
 
 function colorizeGroup (elements) {
@@ -35,24 +42,14 @@ function colorizeGroup (elements) {
 }
 
 function colorizeAll () {
+  // console.time('colorizeAll')
   let elements = document.getElementsByClassName('cwdb-ellipsis')
   elements = [].slice.call(elements)
   let idList = getIdList(elements)
   if (idList) {
     idList.map(id => colorizeGroup(elements.filter(x => x.innerHTML.includes(id))))
   }
+  // console.timeEnd('colorizeAll')
 }
 
-async function bind () {
-  for (let i = 0; i < 10; i++) {
-    await sleep(1000)
-    let container = document.getElementsByClassName('cwdb-log-viewer-table-body')
-    if (container.length && container[0].children.length) {
-      container[0].addEventListener('scroll', colorizeAll)
-      colorizeAll()
-      break
-    }
-  }
-}
-
-bind()
+setInterval(colorizeAll, 1000)
